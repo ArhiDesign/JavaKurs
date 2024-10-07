7ac65a57b542f8590c41342bc008e3f204b831ed
Svetalina <svetalina9@gmail.com>
2024-10-07 19:42:50 +0200

    HomWork18

---
+++ src/homework_18/Calculator.java
@@ -0,0 +1,30 @@
+package homework_18;
+
+public class Calculator {
+
+
+            public static double plus(double a, double b) {
+                return a + b;
+            }
+
+
+            public static double sub(double a, double b) {
+                return a - b;
+            }
+
+
+            public static double mult(double a, double b) {
+                return a * b;
+            }
+
+
+            public static double div(double a, double b) {
+                return a / b;
+            }
+
+
+        }
+
+
+
+
---
+++ src/homework_18/CalculatorApp.java
@@ -0,0 +1,15 @@
+package homework_18;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        double a = 7;
+        double b = 2;
+
+        System.out.println("Сложение: " + Calculator.plus(a, b));
+        System.out.println("Вычитание: " + Calculator.sub(a, b));
+        System.out.println("Умножение: " + Calculator.mult(a, b));
+        System.out.println("Деление: " + Calculator.div(a, b));
+    }
+}
+
+
