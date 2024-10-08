c946eb6a5088e33be58c68a159cd15e38ed9cc6f
Svetalina <svetalina9@gmail.com>
2024-10-08 16:26:07 +0200

    8.10.

---
+++ src/homework_19/Calculator.java
@@ -0,0 +1,32 @@
+package homework_19;
+
+public class Calculator {
+
+
+            public static double plus(double a, double b) {
+                return a + b;
+            }
+
+
+            public static double sub(double a, double b) {
+                return a - b;
+            }
+
+
+            public static double mult(double a, double b) {
+                return a * b;
+            }
+
+
+            public static double div(double a, double b) {
+                return a / b;
+            }
+
+    public static final double PI = 3.141519;
+
+
+        }
+
+
+
+
---
+++ src/homework_19/Task2.java
@@ -0,0 +1,22 @@
+package homework_19;
+/*
+Неизменяемая переменная
+•	Объявите переменную final int MAX_USERS = 100;.
+•	Попробуйте изменить значение MAX_USERS в коде после её инициализации.
+•	Объясните, что произошло при попытке изменения значения.
+     */
+public class Task2 {
+        public static void main(String[] args) {
+            final int MAX_USERS = 100;
+
+
+            }
+
+            // MAX_USERS = 50; //  Ошибка компиляции
+
+
+        }
+
+
+
+
